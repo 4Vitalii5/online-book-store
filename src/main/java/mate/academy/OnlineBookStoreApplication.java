b3f95ed9fcb2df292f6c54bc26aa2c0b9f5917eb
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-25 18:32:13 +0300

    Added @RequiredArgsConstructor annotation and removed @Autowired annotation. Made the class field final. Changed the name of the variable harryPotter to the book.

--- src/main/java/mate/academy/OnlineBookStoreApplication.java
+++ src/main/java/mate/academy/OnlineBookStoreApplication.java
@@ -1,18 +1,18 @@
 package mate.academy;
 
 import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
 import mate.academy.model.Book;
 import mate.academy.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class OnlineBookStoreApplication {
-    @Autowired
-    private BookService bookService;
+    private final BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
@@ -21,15 +21,15 @@ public class OnlineBookStoreApplication {
     @Bean
     public CommandLineRunner commandLineRunner() {
         return args -> {
-            Book harryPotter = new Book();
-            harryPotter.setTitle("Harry Potter");
-            harryPotter.setAuthor("J. K. Rowling");
-            harryPotter.setPrice(BigDecimal.valueOf(250));
-            harryPotter.setDescription("Story about boy in a magical world");
-            harryPotter.setIsbn("978-3-16-148410-0");
-            harryPotter.setCoverImage("A colorful picture of Harry flying in a car");
+            Book book = new Book();
+            book.setTitle("Harry Potter");
+            book.setAuthor("J. K. Rowling");
+            book.setPrice(BigDecimal.valueOf(250));
+            book.setDescription("Story about boy in a magical world");
+            book.setIsbn("978-3-16-148410-0");
+            book.setCoverImage("A colorful picture of Harry flying in a car");
 
-            bookService.save(harryPotter);
+            bookService.save(book);
             System.out.println(bookService.findAll());
         };
     }
