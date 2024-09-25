b91d10c7505cc1ee32956d131e06ecfea9ce3ce0
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-25 18:24:45 +0300

    The @Data annotation has been replaced by the @Getter, @Setter, @Table annotations. Also removed the empty constructor.

--- src/main/java/mate/academy/model/Book.java
+++ src/main/java/mate/academy/model/Book.java
@@ -5,12 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,8 +30,4 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
-
-    public Book() {
-
-    }
 }
