ae1f4c2f45516e3b777893eb5b85ec111b86555b
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-25 18:27:55 +0300

    Added @RequiredArgsConstructor annotation and removed explicit constructor writing with one parameter.

--- src/main/java/mate/academy/repository/impl/BookRepositoryImpl.java
+++ src/main/java/mate/academy/repository/impl/BookRepositoryImpl.java
@@ -1,23 +1,19 @@
 package mate.academy.repository.impl;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.model.Book;
 import mate.academy.repository.BookRepository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory sessionFactory;
-
-    @Autowired
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
 
     @Override
     public Book save(Book book) {
