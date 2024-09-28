5a8af40cfdb1637130a4cd01c357fa06de46f155
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-28 17:09:57 +0300

    Removed unnecessary bean.

--- src/main/java/mate/academy/OnlineBookStoreApplication.java
+++ src/main/java/mate/academy/OnlineBookStoreApplication.java
@@ -1,13 +1,9 @@
 package mate.academy;
 
-import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
-import mate.academy.model.Book;
 import mate.academy.service.BookService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -17,21 +13,4 @@ public class OnlineBookStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book book = new Book();
-            book.setTitle("Harry Potter");
-            book.setAuthor("J. K. Rowling");
-            book.setPrice(BigDecimal.valueOf(250));
-            book.setDescription("Story about boy in a magical world");
-            book.setIsbn("978-3-16-148410-0");
-            book.setCoverImage("A colorful picture of Harry flying in a car");
-
-            bookService.save(book);
-            System.out.println(bookService.findAll());
-        };
-    }
-
 }
