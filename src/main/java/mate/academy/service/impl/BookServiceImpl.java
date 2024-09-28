44994d832c72eff4e25f481ab8ed5bf20680242d
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-28 17:36:51 +0300

    Implemented logic of findById method. Changed return types from Entity to EntityDto

--- src/main/java/mate/academy/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/service/impl/BookServiceImpl.java
@@ -2,6 +2,10 @@ package mate.academy.service.impl;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import mate.academy.dto.BookDto;
+import mate.academy.dto.CreateBookRequestDto;
+import mate.academy.exception.EntityNotFoundException;
+import mate.academy.mapper.EntityMapper;
 import mate.academy.model.Book;
 import mate.academy.repository.BookRepository;
 import mate.academy.service.BookService;
@@ -11,14 +15,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
+    private final EntityMapper entityMapper;
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = entityMapper.toModel(requestDto);
+        bookRepository.save(book);
+        return entityMapper.toDto(book);
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(entityMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public BookDto findById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Can't find book by id: " + id)
+        );
+        return entityMapper.toDto(book);
     }
 }
