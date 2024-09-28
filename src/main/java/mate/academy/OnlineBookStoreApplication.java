31d9341a3aff1a31c5d7c6136fcc638a4afcf461
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-28 17:15:27 +0300

    Removed unnecessary field and annotations.

--- src/main/java/mate/academy/OnlineBookStoreApplication.java
+++ src/main/java/mate/academy/OnlineBookStoreApplication.java
@@ -1,14 +1,10 @@
 package mate.academy;
 
-import lombok.RequiredArgsConstructor;
-import mate.academy.service.BookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class OnlineBookStoreApplication {
-    private final BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
