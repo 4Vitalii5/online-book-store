acd2e0b840b0593cc6f5b35ac826fcdaa3cc151c
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-28 17:36:00 +0300

    Implemented logic of findById method.

--- src/main/java/mate/academy/repository/impl/BookRepositoryImpl.java
+++ src/main/java/mate/academy/repository/impl/BookRepositoryImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.repository.impl;
 
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.model.Book;
@@ -43,6 +44,15 @@ public class BookRepositoryImpl implements BookRepository {
             return session.createQuery("FROM Book", Book.class).getResultList();
         } catch (Exception e) {
             throw new DataProcessingException("Can't get all books", e);
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.get(Book.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get book with id: " + id, e);
         }
     }
 }
