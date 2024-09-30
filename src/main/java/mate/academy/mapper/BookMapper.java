eef396f4732213298f598341a08f8b68fc1f0438
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-30 09:24:37 +0300

    Renamed EntityMapper to BookMapper

--- src/main/java/mate/academy/mapper/BookMapper.java
+++ src/main/java/mate/academy/mapper/BookMapper.java
@@ -7,7 +7,7 @@ import mate.academy.model.Book;
 import org.mapstruct.Mapper;
 
 @Mapper(config = MapperConfig.class)
-public interface EntityMapper {
+public interface BookMapper {
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
