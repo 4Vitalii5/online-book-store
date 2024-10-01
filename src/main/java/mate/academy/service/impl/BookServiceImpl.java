71dc3286683f973f8864d92955132eafc74f665a
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-02 00:59:11 +0300

    Implemented logic for delete and update methods. Added private method to check if book exist in DB

--- src/main/java/mate/academy/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/service/impl/BookServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.dto.BookDto;
 import mate.academy.dto.CreateBookRequestDto;
+import mate.academy.exception.DuplicateResourceException;
 import mate.academy.exception.EntityNotFoundException;
 import mate.academy.mapper.BookMapper;
 import mate.academy.model.Book;
 import mate.academy.repository.BookRepository;
 import mate.academy.service.BookService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,9 +19,14 @@ public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
 
+    @Transactional
     @Override
     public BookDto save(CreateBookRequestDto requestDto) {
         Book book = bookMapper.toModel(requestDto);
+        if (bookRepository.existsByIsbn(book.getIsbn())) {
+            throw new DuplicateResourceException("Book with ISBN " + book.getIsbn()
+                    + " already exists");
+        }
         bookRepository.save(book);
         return bookMapper.toDto(book);
     }
@@ -33,9 +40,35 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto findById(Long id) {
-        Book book = bookRepository.findById(id).orElseThrow(() ->
+        Book book = getBookById(id);
+        return bookMapper.toDto(book);
+    }
+
+    @Override
+    public BookDto updateBookById(Long id, CreateBookRequestDto requestDto) {
+        Book book = getBookById(id);
+        if (!book.getIsbn().equals(requestDto.isbn())) {
+            throw new DuplicateResourceException("Book with ISBN " + requestDto.isbn()
+                    + " already exists");
+        }
+        book.setTitle(requestDto.title());
+        book.setAuthor(requestDto.author());
+        book.setPrice(requestDto.price());
+        book.setIsbn(requestDto.isbn());
+        book.setDescription(requestDto.description());
+        book.setCoverImage(requestDto.coverImage());
+        return bookMapper.toDto(bookRepository.save(book));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        getBookById(id);
+        bookRepository.deleteById(id);
+    }
+
+    private Book getBookById(Long id) {
+        return bookRepository.findById(id).orElseThrow(() ->
                 new EntityNotFoundException("Can't find book by id: " + id)
         );
-        return bookMapper.toDto(book);
     }
 }
