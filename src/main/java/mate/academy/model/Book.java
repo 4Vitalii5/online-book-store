cb528843aa8122c49bf6c98f1858e98ddb21136c
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-02 00:56:01 +0300

    Added annotation and field for soft delete.

--- src/main/java/mate/academy/model/Book.java
+++ src/main/java/mate/academy/model/Book.java
@@ -10,10 +10,14 @@ import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted=false")
 @Table(name = "books")
 public class Book {
     @Id
@@ -29,5 +33,8 @@ public class Book {
     @NonNull
     private BigDecimal price;
     private String description;
+    @Column(name = "cover_image")
     private String coverImage;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
 }
