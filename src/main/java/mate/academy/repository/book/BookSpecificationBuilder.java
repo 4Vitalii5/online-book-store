4984421e19c02f6036455e1774c44670a44a3fb5
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-03 21:17:54 +0300

    Moved magic numbers to constants. Changed the logic of price provider executor.

--- src/main/java/mate/academy/repository/book/BookSpecificationBuilder.java
+++ src/main/java/mate/academy/repository/book/BookSpecificationBuilder.java
@@ -1,6 +1,5 @@
 package mate.academy.repository.book;
 
-import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import mate.academy.dto.BookSearchParameters;
 import mate.academy.model.Book;
@@ -12,24 +11,25 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private static final String TITLE_FIELD = "title";
+    private static final String AUTHOR_FIELD = "author";
+    private static final String PRICE_FIELD = "price";
     private final SpecificationProviderManager<Book> specificationProviderManager;
 
     @Override
     public Specification<Book> build(BookSearchParameters searchParameters) {
         Specification<Book> spec = Specification.where(null);
         if (searchParameters.titles() != null && searchParameters.titles().length > 0) {
-            spec = spec.and(specificationProviderManager.getSpecificationProvider("title")
+            spec = spec.and(specificationProviderManager.getSpecificationProvider(TITLE_FIELD)
                     .getSpecification(searchParameters.titles()));
         }
         if (searchParameters.authors() != null && searchParameters.authors().length > 0) {
-            spec = spec.and(specificationProviderManager.getSpecificationProvider("author")
+            spec = spec.and(specificationProviderManager.getSpecificationProvider(AUTHOR_FIELD)
                     .getSpecification(searchParameters.authors()));
         }
-        if (searchParameters.prices() != null && searchParameters.prices().length == 2) {
-            BigDecimal minPrice = searchParameters.prices()[0];
-            BigDecimal maxPrice = searchParameters.prices()[1];
-            spec = spec.and((root, query, criteriaBuilder) ->
-                    criteriaBuilder.between(root.get("price"), minPrice, maxPrice));
+        if (searchParameters.prices() != null && searchParameters.prices().length > 0) {
+            spec = spec.and(specificationProviderManager.getSpecificationProvider(PRICE_FIELD)
+                    .getSpecification(searchParameters.prices()));
         }
         return spec;
     }
