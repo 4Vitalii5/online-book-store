4d338de52627769258f663469529f918e5dc425c
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-03 21:15:33 +0300

    Added necessary annotations and updateBookFromDto method

--- src/main/java/mate/academy/mapper/BookMapper.java
+++ src/main/java/mate/academy/mapper/BookMapper.java
@@ -5,10 +5,20 @@ import mate.academy.dto.BookDto;
 import mate.academy.dto.CreateBookRequestDto;
 import mate.academy.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
+
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "author", target = "author")
+    @Mapping(source = "isbn", target = "isbn")
+    @Mapping(source = "price", target = "price")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "coverImage", target = "coverImage")
+    Book updateBookFromDto(CreateBookRequestDto requestDto, @MappingTarget Book book);
 }
