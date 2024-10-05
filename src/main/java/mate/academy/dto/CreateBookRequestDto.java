a3d87af6a4db7adf39292f89ab9ba49949158685
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-05 20:12:51 +0300

    Added necessary annotation for validation.

--- src/main/java/mate/academy/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dto/CreateBookRequestDto.java
@@ -1,7 +1,22 @@
 package mate.academy.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import mate.academy.validation.Isbn;
 
-public record CreateBookRequestDto(String title, String author, String isbn, BigDecimal price,
-                                   String description, String coverImage) {
+public record CreateBookRequestDto(
+        @NotNull
+        String title,
+        @NotNull
+        String author,
+        @NotNull
+        @Isbn
+        String isbn,
+        @NotNull
+        @Min(0)
+        BigDecimal price,
+        String description,
+        String coverImage
+) {
 }
