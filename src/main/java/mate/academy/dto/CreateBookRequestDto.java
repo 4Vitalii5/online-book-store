4d6a742267830238478d1484b394de91eba4c1fa
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-06 20:00:00 +0300

    Changed annotations NotNull to NotBlank

--- src/main/java/mate/academy/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dto/CreateBookRequestDto.java
@@ -1,16 +1,16 @@
 package mate.academy.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import mate.academy.validation.Isbn;
 
 public record CreateBookRequestDto(
-        @NotNull
+        @NotBlank
         String title,
-        @NotNull
+        @NotBlank
         String author,
-        @NotNull
         @Isbn
         String isbn,
         @NotNull
