6fb807455e6aac9003cea31b27d26e76aafee06c
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-15 15:27:49 +0300

    Removed UsernameNotFoundException from method declaration. Changed UsernameNotFoundException to EntityNotFoundException

--- src/main/java/mate/academy/security/CustomUserDetailsService.java
+++ src/main/java/mate/academy/security/CustomUserDetailsService.java
@@ -1,10 +1,10 @@
 package mate.academy.security;
 
 import lombok.RequiredArgsConstructor;
+import mate.academy.exception.EntityNotFoundException;
 import mate.academy.repository.user.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +13,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) {
         return userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Can't find user by email: "
+                .orElseThrow(() -> new EntityNotFoundException("Can't find user by email: "
                         + username));
     }
 }
