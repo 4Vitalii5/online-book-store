7245cbaa12edb4a71c333a56581325af1f73003b
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-18 17:04:11 +0300

    Implemented soft deleted

--- src/main/java/mate/academy/model/Role.java
+++ src/main/java/mate/academy/model/Role.java
@@ -10,11 +10,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Getter
 @Setter
+@SQLDelete(sql = "UPDATE roles SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted=false")
 @Table(name = "roles")
 public class Role implements GrantedAuthority {
     @Id
@@ -23,6 +27,8 @@ public class Role implements GrantedAuthority {
     @Column(nullable = false, unique = true)
     @Enumerated(EnumType.STRING)
     private RoleName role;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 
     @Override
     public String getAuthority() {
