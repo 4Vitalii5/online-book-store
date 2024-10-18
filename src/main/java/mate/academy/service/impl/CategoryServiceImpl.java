92baf8c98995bb64261e47d67d8131d9f383d3b7
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-18 17:18:44 +0300

    Created implementation of category service and implemented the logic of the CRUD method.

---
+++ src/main/java/mate/academy/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,68 @@
+package mate.academy.service.impl;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.dto.category.CategoryDto;
+import mate.academy.exception.DuplicateResourceException;
+import mate.academy.exception.EntityNotFoundException;
+import mate.academy.mapper.CategoryMapper;
+import mate.academy.model.Category;
+import mate.academy.repository.category.CategoryRepository;
+import mate.academy.service.CategoryService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    @Override
+    public CategoryDto save(CategoryDto requestDto) {
+        Category category = categoryMapper.toEntity(requestDto);
+        if (categoryRepository.findByName(category.getName()) != null) {
+            throw new DuplicateResourceException("Category with name:"
+                    + category.getName()
+                    + " already exists");
+        }
+        categoryRepository.save(category);
+        return categoryMapper.toDto(category);
+    }
+
+    @Override
+    public List<CategoryDto> findAll(Pageable pageable) {
+        return categoryRepository.findAll(pageable).stream()
+                .map(categoryMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public CategoryDto getById(Long id) {
+        Category category = getCategoryById(id);
+        return categoryMapper.toDto(category);
+    }
+
+    @Override
+    public CategoryDto update(Long id, CategoryDto categoryDto) {
+        Category category = getCategoryById(id);
+        if (!categoryRepository.findByName(category.getName()).getId().equals(id)) {
+            throw new DuplicateResourceException("Category with name:"
+                    + category.getName()
+                    + " already exists");
+        }
+        categoryMapper.updateCategoryFromDto(categoryDto, category);
+        return categoryMapper.toDto(categoryRepository.save(category));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    private Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Can't find category by id: " + id)
+        );
+    }
+}
