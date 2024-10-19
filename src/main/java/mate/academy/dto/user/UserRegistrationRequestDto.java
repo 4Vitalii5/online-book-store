1a6dbf11d3865284b5809a9d18c04edab56fd190
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-19 21:20:25 +0300

    Replaced NotBlank to NotEmpty

--- src/main/java/mate/academy/dto/user/UserRegistrationRequestDto.java
+++ src/main/java/mate/academy/dto/user/UserRegistrationRequestDto.java
@@ -2,6 +2,7 @@ package mate.academy.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import mate.academy.validation.FieldMatch;
 import org.hibernate.validator.constraints.Length;
 
@@ -13,10 +14,10 @@ import org.hibernate.validator.constraints.Length;
         )
 })
 public record UserRegistrationRequestDto(
-        @NotBlank
+        @NotEmpty
         @Email
         String email,
-        @NotBlank
+        @NotEmpty
         @Length(min = 8, max = 20)
         String password,
         @NotBlank
