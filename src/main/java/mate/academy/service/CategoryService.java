72e210483bb8e07e307d26b19c62010dd9f30e23
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-19 21:23:35 +0300

    CategoryDto is replaced by CreateCategoryRequestDto

--- src/main/java/mate/academy/service/CategoryService.java
+++ src/main/java/mate/academy/service/CategoryService.java
@@ -2,16 +2,17 @@ package mate.academy.service;
 
 import java.util.List;
 import mate.academy.dto.category.CategoryDto;
+import mate.academy.dto.category.CreateCategoryRequestDto;
 import org.springframework.data.domain.Pageable;
 
 public interface CategoryService {
-    CategoryDto save(CategoryDto requestDto);
+    CategoryDto save(CreateCategoryRequestDto requestDto);
 
     List<CategoryDto> findAll(Pageable pageable);
 
     CategoryDto getById(Long id);
 
-    CategoryDto update(Long id, CategoryDto categoryDto);
+    CategoryDto update(Long id, CreateCategoryRequestDto requestDto);
 
     void deleteById(Long id);
 }
