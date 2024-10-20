4952b78155ea7cbb989e5660abf4cd9f261d63f8
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-20 11:50:34 +0300

    Changed NotEmpty annotation to NotBlank and used Size for both fields.

--- src/main/java/mate/academy/dto/category/CreateCategoryRequestDto.java
+++ src/main/java/mate/academy/dto/category/CreateCategoryRequestDto.java
@@ -1,10 +1,14 @@
 package mate.academy.dto.category;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CreateCategoryRequestDto(
-        @NotEmpty
+        @NotBlank
+
+        @Size(min = 3, max = 100)
         String name,
+        @Size(min = 8, max = 1000)
         String description
 ) {
 }
