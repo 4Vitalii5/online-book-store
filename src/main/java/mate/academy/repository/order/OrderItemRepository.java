4b56b214be5d414bac2cd517f1f0c7aa016109bc
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-10-29 14:07:55 +0200

    The name of the method has been renamed.

--- src/main/java/mate/academy/repository/order/OrderItemRepository.java
+++ src/main/java/mate/academy/repository/order/OrderItemRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     List<OrderItem> findByOrderId(Long orderId, Pageable pageable);
 
-    Optional<OrderItem> findByOrderIdAndId(Long orderId, Long itemId);
+    Optional<OrderItem> findByIdAndOrderId(Long itemId, Long orderId);
 }
