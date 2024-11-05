56619009d80e9baf4a8ee65585596f9bfe8981fa
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-11-06 00:22:47 +0200

    Added a constructor to simplify the creation of objects

--- src/main/java/mate/academy/dto/book/BookDto.java
+++ src/main/java/mate/academy/dto/book/BookDto.java
@@ -16,4 +16,16 @@ public class BookDto {
     private String description;
     private String coverImage;
     private List<Long> categoryIds;
+
+    public BookDto(Long id, String title, String author, String isbn, BigDecimal price,
+                   String coverImage, String description, List<Long> categoryIds) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+        this.coverImage = coverImage;
+        this.description = description;
+        this.categoryIds = categoryIds;
+    }
 }
