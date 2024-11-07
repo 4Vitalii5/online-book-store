e5997912288530f894c015c44f03ae66f910ff2e
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-11-07 15:38:33 +0200

    Moved scripts under the test class. The creation of standard objects has been moved to TestUtil. All constants have been moved to TestConstants.

--- src/test/java/mate/academy/controller/BookControllerTest.java
+++ src/test/java/mate/academy/controller/BookControllerTest.java
@@ -1,28 +1,16 @@
 package mate.academy.controller;
 
-import static mate.academy.constant.BookTestConstants.NEW_BOOK_AUTHOR;
-import static mate.academy.constant.BookTestConstants.NEW_BOOK_CATEGORY_IDS;
-import static mate.academy.constant.BookTestConstants.NEW_BOOK_COVER_IMAGE;
-import static mate.academy.constant.BookTestConstants.NEW_BOOK_DESCRIPTION;
-import static mate.academy.constant.BookTestConstants.NEW_BOOK_ISBN;
-import static mate.academy.constant.BookTestConstants.NEW_BOOK_PRICE;
-import static mate.academy.constant.BookTestConstants.NEW_BOOK_TITLE;
-import static mate.academy.constant.BookTestConstants.SEARCH_AUTHOR;
-import static mate.academy.constant.BookTestConstants.SEARCH_PARAM_AUTHORS;
-import static mate.academy.constant.BookTestConstants.SEARCH_PARAM_PRICES;
-import static mate.academy.constant.BookTestConstants.SEARCH_PARAM_TITLES;
-import static mate.academy.constant.BookTestConstants.SEARCH_PRICE_MAX;
-import static mate.academy.constant.BookTestConstants.SEARCH_PRICE_MIN;
-import static mate.academy.constant.BookTestConstants.SEARCH_TITLE;
-import static mate.academy.constant.BookTestConstants.UPDATED_BOOK_AUTHOR;
-import static mate.academy.constant.BookTestConstants.UPDATED_BOOK_CATEGORY_IDS;
-import static mate.academy.constant.BookTestConstants.UPDATED_BOOK_COVER_IMAGE;
-import static mate.academy.constant.BookTestConstants.UPDATED_BOOK_DESCRIPTION;
-import static mate.academy.constant.BookTestConstants.UPDATED_BOOK_ISBN;
-import static mate.academy.constant.BookTestConstants.UPDATED_BOOK_PRICE;
-import static mate.academy.constant.BookTestConstants.UPDATED_BOOK_TITLE;
-import static mate.academy.constant.BookTestConstants.VALID_FIRST_BOOK_ID;
-import static mate.academy.constant.BookTestConstants.VALID_SECOND_BOOK_ID;
+import static mate.academy.constant.TestConstants.SEARCH_AUTHOR;
+import static mate.academy.constant.TestConstants.SEARCH_PARAM_AUTHORS;
+import static mate.academy.constant.TestConstants.SEARCH_PARAM_PRICES;
+import static mate.academy.constant.TestConstants.SEARCH_PARAM_TITLES;
+import static mate.academy.constant.TestConstants.SEARCH_PRICE_MAX;
+import static mate.academy.constant.TestConstants.SEARCH_PRICE_MIN;
+import static mate.academy.constant.TestConstants.SEARCH_TITLE;
+import static mate.academy.constant.TestConstants.VALID_FIRST_BOOK_ID;
+import static mate.academy.constant.TestConstants.VALID_SECOND_BOOK_ID;
+import static mate.academy.constant.TestUtil.CREATE_BOOK_REQUEST_DTO;
+import static mate.academy.constant.TestUtil.UPDATE_BOOK_REQUEST_DTO;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -33,8 +21,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import mate.academy.dto.book.BookDto;
-import mate.academy.dto.book.BookSearchParameters;
-import mate.academy.dto.book.CreateBookRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,8 +33,16 @@ import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@Sql(scripts = "classpath:database/clean-database.sql",
-        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql(scripts = "classpath:database/clean-database.sql")
+@Sql(scripts = {
+        "classpath:database/books/add-books.sql",
+        "classpath:database/categories/add-categories.sql",
+        "classpath:database/books-categories/set-books-to-categories.sql"
+}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = {
+        "classpath:database/books/remove-books.sql",
+        "classpath:database/categories/remove-categories.sql"
+}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class BookControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -58,52 +52,25 @@ public class BookControllerTest {
     @Test
     @DisplayName("Verify creation of new book")
     @WithMockUser(roles = "ADMIN")
-    @Sql(scripts = {
-            "classpath:database/books/add-books.sql",
-            "classpath:database/categories/add-categories.sql",
-            "classpath:database/books-categories/set-books-to-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = {
-            "classpath:database/books/remove-books.sql",
-            "classpath:database/categories/remove-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void createBook_withValidInput_returnsCreatedBook() throws Exception {
         // Given
-        CreateBookRequestDto requestDto = new CreateBookRequestDto(
-                NEW_BOOK_TITLE,
-                NEW_BOOK_AUTHOR,
-                NEW_BOOK_ISBN,
-                NEW_BOOK_PRICE,
-                NEW_BOOK_DESCRIPTION,
-                NEW_BOOK_COVER_IMAGE,
-                NEW_BOOK_CATEGORY_IDS
-        );
         // When
         MvcResult mvcResult = mockMvc.perform(post("/books")
                         .with(csrf())
                         .contentType("application/json")
-                        .content(objectMapper.writeValueAsString(requestDto)))
+                        .content(objectMapper.writeValueAsString(CREATE_BOOK_REQUEST_DTO)))
                 .andExpect(status().isOk())
                 .andReturn();
         // Then
         String jsonResponse = mvcResult.getResponse().getContentAsString();
         BookDto responseDto = objectMapper.readValue(jsonResponse, BookDto.class);
         assertThat(responseDto).isNotNull();
-        assertThat(responseDto.getTitle()).isEqualTo(NEW_BOOK_TITLE);
+        assertThat(responseDto.getTitle()).isEqualTo(CREATE_BOOK_REQUEST_DTO.title());
     }
 
     @Test
     @DisplayName("Get all books")
     @WithMockUser(roles = {"USER", "ADMIN"})
-    @Sql(scripts = {
-            "classpath:database/books/add-books.sql",
-            "classpath:database/categories/add-categories.sql",
-            "classpath:database/books-categories/set-books-to-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = {
-            "classpath:database/books/remove-books.sql",
-            "classpath:database/categories/remove-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void getAllBooks_withValidRequest_returnsBooks() throws Exception {
         // When
         MvcResult mvcResult = mockMvc.perform(get("/books"))
@@ -118,15 +85,6 @@ public class BookControllerTest {
     @Test
     @DisplayName("Get book by id")
     @WithMockUser(roles = {"USER", "ADMIN"})
-    @Sql(scripts = {
-            "classpath:database/books/add-books.sql",
-            "classpath:database/categories/add-categories.sql",
-            "classpath:database/books-categories/set-books-to-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = {
-            "classpath:database/books/remove-books.sql",
-            "classpath:database/categories/remove-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void getBookById_withValidId_returnsBook() throws Exception {
         // When
         MvcResult mvcResult = mockMvc.perform(get("/books/{id}", VALID_FIRST_BOOK_ID))
@@ -142,52 +100,25 @@ public class BookControllerTest {
     @Test
     @DisplayName("Update book by id")
     @WithMockUser(roles = "ADMIN")
-    @Sql(scripts = {
-            "classpath:database/books/add-books.sql",
-            "classpath:database/categories/add-categories.sql",
-            "classpath:database/books-categories/set-books-to-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = {
-            "classpath:database/books/remove-books.sql",
-            "classpath:database/categories/remove-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void updateBook_withValidId_returnsUpdatedBook() throws Exception {
         // Given
-        CreateBookRequestDto requestDto = new CreateBookRequestDto(
-                UPDATED_BOOK_TITLE,
-                UPDATED_BOOK_AUTHOR,
-                UPDATED_BOOK_ISBN,
-                UPDATED_BOOK_PRICE,
-                UPDATED_BOOK_DESCRIPTION,
-                UPDATED_BOOK_COVER_IMAGE,
-                UPDATED_BOOK_CATEGORY_IDS
-        );
         // When
         MvcResult mvcResult = mockMvc.perform(put("/books/{id}", VALID_SECOND_BOOK_ID)
                         .with(csrf())
                         .contentType("application/json")
-                        .content(objectMapper.writeValueAsString(requestDto)))
+                        .content(objectMapper.writeValueAsString(UPDATE_BOOK_REQUEST_DTO)))
                 .andExpect(status().isOk())
                 .andReturn();
         // Then
         String jsonResponse = mvcResult.getResponse().getContentAsString();
         BookDto responseDto = objectMapper.readValue(jsonResponse, BookDto.class);
         assertThat(responseDto).isNotNull();
-        assertThat(responseDto.getTitle()).isEqualTo(UPDATED_BOOK_TITLE);
+        assertThat(responseDto.getTitle()).isEqualTo(UPDATE_BOOK_REQUEST_DTO.title());
     }
 
     @Test
     @DisplayName("Delete book by id")
     @WithMockUser(roles = "ADMIN")
-    @Sql(scripts = {
-            "classpath:database/books/add-books.sql",
-            "classpath:database/categories/add-categories.sql",
-            "classpath:database/books-categories/set-books-to-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = {
-            "classpath:database/books/remove-books.sql",
-            "classpath:database/categories/remove-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void deleteBook_withValidId_deletesBook() throws Exception {
         // When
         mockMvc.perform(delete("/books/{id}", VALID_SECOND_BOOK_ID)
@@ -201,32 +132,15 @@ public class BookControllerTest {
     @Test
     @DisplayName("Search books by parameters")
     @WithMockUser(roles = {"USER", "ADMIN"})
-    @Sql(scripts = {
-            "classpath:database/books/add-books.sql",
-            "classpath:database/categories/add-categories.sql",
-            "classpath:database/books-categories/set-books-to-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = {
-            "classpath:database/books/remove-books.sql",
-            "classpath:database/categories/remove-categories.sql"
-    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void searchBooks_withValidParameters_returnsBooks() throws Exception {
         // Given
-        BookSearchParameters searchParameters = new BookSearchParameters(
-                new String[]{SEARCH_TITLE},
-                new String[]{SEARCH_AUTHOR},
-                new String[]{
-                        SEARCH_PRICE_MIN,
-                        SEARCH_PRICE_MAX
-                }
-        );
         // When
         MvcResult mvcResult = mockMvc.perform(get("/books/search")
                         .param(SEARCH_PARAM_TITLES, SEARCH_TITLE)
                         .param(SEARCH_PARAM_AUTHORS, SEARCH_AUTHOR)
-                        .param(SEARCH_PARAM_PRICES, SEARCH_PRICE_MIN,
-                                SEARCH_PRICE_MAX))
-                .andExpect(status().isOk()).andReturn();
+                        .param(SEARCH_PARAM_PRICES, SEARCH_PRICE_MIN, SEARCH_PRICE_MAX))
+                .andExpect(status().isOk())
+                .andReturn();
         // Then
         String jsonResponse = mvcResult.getResponse().getContentAsString();
         BookDto[] responseDtos = objectMapper.readValue(jsonResponse, BookDto[].class);
