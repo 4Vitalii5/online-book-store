faa334ff1bb44638c3e4742680fe32648a19bcdf
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-11-07 15:33:01 +0200

    Removed the constructor and used the lombok annotation.

--- src/main/java/mate/academy/dto/book/BookDto.java
+++ src/main/java/mate/academy/dto/book/BookDto.java
@@ -2,11 +2,13 @@ package mate.academy.dto.book;
 
 import java.math.BigDecimal;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class BookDto {
     private Long id;
     private String title;
@@ -16,16 +18,4 @@ public class BookDto {
     private String description;
     private String coverImage;
     private List<Long> categoryIds;
-
-    public BookDto(Long id, String title, String author, String isbn, BigDecimal price,
-                   String coverImage, String description, List<Long> categoryIds) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.price = price;
-        this.coverImage = coverImage;
-        this.description = description;
-        this.categoryIds = categoryIds;
-    }
 }
