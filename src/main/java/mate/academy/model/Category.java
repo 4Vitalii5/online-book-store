efd541120123d3931c0db06088275f5361301057
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-11-07 15:33:18 +0200

    Removed the constructor and used the lombok annotation.

--- src/main/java/mate/academy/model/Category.java
+++ src/main/java/mate/academy/model/Category.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import org.hibernate.annotations.SQLRestriction;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
 @SQLRestriction("is_deleted=false")
 @Table(name = "categories")
@@ -28,10 +30,4 @@ public class Category {
     private String description;
     @Column(nullable = false)
     private boolean isDeleted = false;
-
-    public Category(Long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
 }
