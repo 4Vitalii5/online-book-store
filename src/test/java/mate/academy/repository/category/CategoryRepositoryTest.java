b4d9668fdca962856d9f27e3775a2e7dc8084dd1
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-11-07 15:40:33 +0200

    Moved scripts under the CategoryRepositoryTest class. The creation of standard objects has been moved to TestUtil. All constants have been moved to TestConstants.

--- src/test/java/mate/academy/repository/category/CategoryRepositoryTest.java
+++ src/test/java/mate/academy/repository/category/CategoryRepositoryTest.java
@@ -1,7 +1,7 @@
 package mate.academy.repository.category;
 
-import static mate.academy.constant.CategoryTestConstants.INVALID_CATEGORY_NAME;
-import static mate.academy.constant.CategoryTestConstants.SAMPLE_CATEGORY_NAME;
+import static mate.academy.constant.TestConstants.INVALID_CATEGORY_NAME;
+import static mate.academy.constant.TestUtil.FIRST_CATEGORY;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import mate.academy.model.Category;
@@ -14,33 +14,28 @@ import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Sql(scripts = "classpath:database/clean-database.sql",
-        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql(scripts = "classpath:database/clean-database.sql")
+@Sql(scripts = "classpath:database/categories/add-categories.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "classpath:database/categories/remove-categories.sql",
+        executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class CategoryRepositoryTest {
     @Autowired
     private CategoryRepository categoryRepository;
 
     @Test
     @DisplayName("Verify findByName() method works")
-    @Sql(scripts = "classpath:database/categories/add-categories.sql",
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = "classpath:database/categories/remove-categories.sql",
-            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void findByName_validName_returnsCategory() {
         // When
-        Category foundCategory = categoryRepository.findByName(SAMPLE_CATEGORY_NAME);
+        Category foundCategory = categoryRepository.findByName(FIRST_CATEGORY.getName());
         String actual = foundCategory.getName();
         // Then
         assertThat(foundCategory).isNotNull();
-        assertThat(actual).isEqualTo(SAMPLE_CATEGORY_NAME);
+        assertThat(actual).isEqualTo(FIRST_CATEGORY.getName());
     }
 
     @Test
     @DisplayName("Verify findByName() method with invalid name")
-    @Sql(scripts = "classpath:database/categories/add-categories.sql",
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = "classpath:database/categories/remove-categories.sql",
-            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void findByName_invalidName_returnsNull() {
         // When
         Category foundCategory = categoryRepository.findByName(INVALID_CATEGORY_NAME);
