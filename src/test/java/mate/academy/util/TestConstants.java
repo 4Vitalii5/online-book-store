8ca5920de47437eef08d22fd24fa76c40f059505
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-11-12 21:04:23 +0200

    Added all necessary constants.

--- src/test/java/mate/academy/util/TestConstants.java
+++ src/test/java/mate/academy/util/TestConstants.java
@@ -1,7 +1,11 @@
 package mate.academy.util;
 
+import static mate.academy.model.Role.RoleName.ROLE_SUPER_ADMIN;
+
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import mate.academy.model.Role;
 
 public class TestConstants {
     // Data for book creation
@@ -64,6 +68,49 @@ public class TestConstants {
     public static final long SECOND_CATEGORY_ID = 3L;
     public static final String SECOND_CATEGORY_NAME = "Non-fiction";
     public static final String SECOND_CATEGORY_DESCRIPTION = "Documentary books";
+    //User
+    public static final Long USER_ID = 1L;
+    public static final Long INVALID_USER_ID = 23L;
+    public static final String USER_EMAIL = "sashko45@example.com";
+    public static final String VALID_USER_EMAIL = "testuser@example.com";
+    public static final String NON_EXISTING_USER_EMAIL = "maks43@ukr.com";
+    public static final String USER_PASSWORD = "password123";
+    public static final String USER_FIRST_NAME = "John";
+    public static final String USER_LAST_NAME = "Doe";
+    public static final String USER_SHIPPING_ADDRESS = "123 Main St, Springfield";
+    public static final String ENCODED_PASSWORD = "$2a$10$7d7q7q6/8Vh2Q4H1OpXJNe";
+    public static final Role.RoleName DEFAULT_ROLE = Role.RoleName.ROLE_USER;
+    public static final Role.RoleName INVALID_ROLE = ROLE_SUPER_ADMIN;
+    public static final String DUPLICATE_EMAIL_MESSAGE = "User with email: sashko45@example.com "
+            + "already exists";
+    public static final String ROLE_NOT_FOUND_MESSAGE = "Role: ROLE_USER not found";
+    //Order
+    public static final Long ORDER_ID = 1L;
+    public static final Long ITEM_ID = 1L;
+    public static final Long SECOND_ITEM_ID = 2L;
+    public static final Long BOOK_ID = 1L;
+    public static final Long SECOND_BOOK_ID = 1L;
+    public static final String SHIPPING_ADDRESS = "123 Main St, Springfield";
+    public static final String NEW_ORDER_STATUS = "NEW";
+    public static final BigDecimal ITEM_PRICE = BigDecimal.valueOf(10.00);
+    public static final int ITEM_QUANTITY = 2;
+    public static final int SECOND_ITEM_QUANTITY = 10;
+    public static final BigDecimal TOTAL_PRICE = ITEM_PRICE.multiply(
+            BigDecimal.valueOf(ITEM_QUANTITY)
+    );
+    public static final LocalDateTime ORDER_DATE = LocalDateTime.now();
+    public static final String ORDER_NOT_FOUND_MESSAGE = "Can't find order with id:" + ORDER_ID;
+    public static final String ORDER_PROCESSING_EXCEPTION_MESSAGE = "Can't create order. "
+            + "CartItems is empty";
+    //Shopping Cart
+    public static final Long SHOPPING_CART_ID = 1L;
+    public static final Long CART_ITEM_ID = 1L;
+    public static final String CART_NOT_FOUND_MESSAGE = "Can't find cart item with id:"
+            + CART_ITEM_ID;
+
+    public static final Long INVALID_ORDER_ID = 99L;
+    public static final Long NON_EXISTING_ID = 999L;
+    public static final Long NON_EXISTING_ORDER_ID = 999L;
 
     private TestConstants() {
     }
