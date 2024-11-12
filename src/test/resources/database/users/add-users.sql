f8d82e4794852b071fca723fcbb968bd53fefd8c
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-11-12 20:47:37 +0200

    Created a sql script that inserts the users.

---
+++ src/test/resources/database/users/add-users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, email, password, first_name, last_name, shipping_address, is_deleted)
+VALUES
+    (1, 'testuser@example.com', '$2a$12$NWIYhIQUnpt9uHwdtA/RK.E7XMIgwmPgjJLMArJnQBK/qAiqy.Eq6', 'John', 'Doe', '123 Main St', false),
+    (2, 'newuser@example.com', '$2a$12$NWIYhIQUnpt9uHwdtA/RK.E7XMIgwmPgjJLMArJnQBK/qAiqy.Eq6', 'Jane', 'Smith', '456 Elm St', false);
