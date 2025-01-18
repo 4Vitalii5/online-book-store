24bec2bb5c2328b512987857e5e8130a04fc547d
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-01-18 12:16:21 +0200

    Removed blank line

--- src/main/java/mate/academy/mapper/BookMapper.java
+++ src/main/java/mate/academy/mapper/BookMapper.java
@@ -13,7 +13,6 @@ import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
-
     BookDto toDto(Book book);
 
     Book toEntity(CreateBookRequestDto requestDto);
