6ccde1725db7000e75aae13ba45b3bcad01b717b
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-01-18 12:17:44 +0200

    Replaced Min(0) to Positive

--- src/main/java/mate/academy/dto/book/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dto/book/CreateBookRequestDto.java
@@ -1,9 +1,9 @@
 package mate.academy.dto.book;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.util.List;
 import mate.academy.validation.Isbn;
@@ -16,7 +16,7 @@ public record CreateBookRequestDto(
         @Isbn
         String isbn,
         @NotNull
-        @Min(0)
+        @Positive
         BigDecimal price,
         String description,
         String coverImage,
